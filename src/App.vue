dd443bc38f54c053e5d148ce554e43e9782239f6
Ashley Shang <ashley@acube.com.tw>
2024-06-07 13:55:51 +0800

    feat: 新增countdown feature

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,65 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <button :disabled="isButtonDisabled" @click="handleClick">
+      {{ buttonText }}
+    </button>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref, computed, onMounted } from 'vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+const countdown = ref(0);
+const timer = ref(null);
+
+const isButtonDisabled = computed(() => countdown.value > 0);
+const buttonText = computed(() => countdown.value > 0 ? `${countdown.value} 秒後可再次點擊` : '獲取驗證碼');
+
+const handleClick = () => {
+  startCountdown();
+};
+
+const startCountdown = () => {
+  countdown.value = 30;
+  saveCountdown();
+  timer.value = setInterval(() => {
+    countdown.value--;
+    saveCountdown();
+    if (countdown.value <= 0) {
+      clearInterval(timer.value);
+    }
+  }, 1000);
+};
+
+const saveCountdown = () => {
+  const expireTime = Date.now() + countdown.value * 1000;
+  localStorage.setItem('countdownExpireTime', expireTime);
+};
+
+const loadCountdown = () => {
+  const expireTime = localStorage.getItem('countdownExpireTime');
+  if (expireTime) {
+    const remainingTime = Math.ceil((expireTime - Date.now()) / 1000);
+    if (remainingTime > 0) {
+      countdown.value = remainingTime;
+      timer.value = setInterval(() => {
+        countdown.value--;
+        if (countdown.value <= 0) {
+          clearInterval(timer.value);
+        }
+      }, 1000);
+    }
   }
-}
+};
+
+onMounted(() => {
+  loadCountdown();
+});
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+button {
+  padding: 10px 20px;
+  font-size: 16px;
 }
 </style>
